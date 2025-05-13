e2c8bb356244eb4a05d7f85e42e43c0f7329d971
STEELLANGEL <steallangel@mail.ru>
2025-05-14 02:49:42 +0500

    Добавил библиотеку классов

---
+++ RaceCommon/User.cs
@@ -0,0 +1,14 @@
+﻿namespace RaceCommon
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public User(string name)
+        {
+            Name = name;
+            Score = 0;
+        }
+    }
+}
---
+++ RaceCommon/UserManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace RaceCommon
+{
+    public class UserManager
+    {
+        public static string path = "userResults.json";
+
+        public static void Append(User user)
+        {
+            var usersResults = GetAll();
+            usersResults.Add(user);
+            Save(usersResults);
+        }
+
+        public static List<User> GetAll()
+        {
+            if (!FileProvider.Exists(path))
+            {
+                return new List<User>();
+            }
+           
+            var fileData = FileProvider.GetValue(path);
+
+            return  JsonConvert.DeserializeObject<List<User>>(fileData);
+        }
+        static void Save(List<User> usersResults)
+        {
+            var jsonData = JsonConvert.SerializeObject(usersResults, Formatting.Indented);
+            FileProvider.Replace(path, jsonData);
+        }
+
+    }
+}
