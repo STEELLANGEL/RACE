7f2531a9327b6d608ea1a4fac9238527c2d8652e
STEELLANGEL <steallangel@mail.ru>
2025-05-14 05:34:46 +0500

    Удалил все неиспользуемые using

--- RaceGame/ResultForm.cs
+++ RaceGame/ResultForm.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using RaceCommon;
+﻿using RaceCommon;
 
 namespace Race
 {
--- RaceGame/StartForm.cs
+++ RaceGame/StartForm.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using RaceCommon;
-
-namespace Race
+﻿namespace Race
 {
     public partial class startForm : Form
     {
