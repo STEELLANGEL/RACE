f0db19c1db2c8eb736f19afe963d1081050f854e
STEELLANGEL <steallangel@mail.ru>
2025-05-14 05:22:17 +0500

    Убрал лишние using

--- RaceGame/RaceGame.cs
+++ RaceGame/RaceGame.cs
@@ -1,17 +1,4 @@
-﻿using Race;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using RaceCommon;
-using static System.Net.Mime.MediaTypeNames;
+﻿using RaceCommon;
 
 namespace Race
 {
